3f125701c15279aff3f497390b72ce72407007d7
Leonardo Pieraccioli <leonardo.pieraccioli@gmail.com>
2023-11-07 11:21:41 +0100

    add teacher model

--- client/src/models/Teacher.js
+++ client/src/models/Teacher.js
@@ -1,3 +1,8 @@
 export default function Teacher(id, surname, name, email, cod_group, cod_department) {
-
+    this.id = id;
+    this.surname = surname;
+    this.name = name;
+    this.email = email;
+    this.cod_group = cod_group;
+    this.cod_department = cod_department;
 }
