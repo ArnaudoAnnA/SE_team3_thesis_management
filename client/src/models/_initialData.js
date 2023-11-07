08292da2bc8a7c5a7fe2b646bd761e5768d050aa
Leonardo Pieraccioli <leonardo.pieraccioli@gmail.com>
2023-11-07 11:37:35 +0100

    add initalization data for students

---
+++ client/src/models/_initialData.js
@@ -0,0 +1,19 @@
+import Application from './Application';
+import Student from './Student';
+import Teacher from './Teacher';
+import Career from './Career';
+import ThesisProposal from './ThesisProposal';
+import Degree from './Degree';
+
+export const students = [
+    new Student("s123456", "Rossi", "Giuseppe", "Male", "Italian", "s123456@studenti.polito.it", "AB123CD", "2019"),
+    new Student("s234567", "Bianchi", "Francesca", "Female", "Italian", "s234567@studenti.polito.it", "EF456GH", "2021"),
+    new Student("s345678", "Ricci", "Luca", "Male", "Italian", "s345678@studenti.polito.it", "IJ789KL", "2023"),
+    new Student("s456789", "Romano", "Valentina", "Female", "Italian", "s456789@studenti.polito.it", "MN012OP", "2020"),
+    new Student("s567890", "Ferrari", "Marco", "Male", "Italian", "s567890@studenti.polito.it", "QR345ST", "2022"),
+    new Student("s678901", "Martini", "Alessia", "Female", "Italian", "s678901@studenti.polito.it", "UV678WX", "2021"),
+    new Student("s789012", "Conti", "Roberto", "Male", "Italian", "s789012@studenti.polito.it", "YZ901AB", "2023"),
+    new Student("s890123", "Gallo", "Laura", "Female", "Italian", "s890123@studenti.polito.it", "CD234EF", "2020"),
+    new Student("s901234", "Marino", "Simone", "Male", "Italian", "s901234@studenti.polito.it", "GH567IJ", "2022"),
+    new Student("s012345", "Ferrara", "Elena", "Female", "Italian", "s012345@studenti.polito.it", "KL890MN", "2021")
+];
