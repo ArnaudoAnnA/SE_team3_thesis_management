27f8889e791a6eeff4873810d2ea9f99175c0fb6
Emilio Cassaro <s319083@studenti.polito.it>
2023-11-13 16:39:58 +0100

    Fixing Console Errors + correcting test

--- client/src/components/InsertProposalForm.jsx
+++ client/src/components/InsertProposalForm.jsx
@@ -188,7 +188,7 @@ function InsertProposalForm(props) {
             onClose={() => setErrorMsg('')}
             dismissible
           >
-              <svg xmlns="http://www.w3.org/2000/svg" style={{margin: "2%"}} width="5%" height="5%" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" style={{margin: "2%"}} width="5%" height="5%" fill="currentColor" className="bi bi-exclamation-triangle" viewBox="0 0 16 16">
                 <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z" />
                 <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z" />
               </svg>
@@ -197,37 +197,35 @@ function InsertProposalForm(props) {
         ) : null}
         <Form onSubmit={handleSubmit} >
            
-            <div class="container" style={{marginBottom: "20px"}}>
+            <div className="container" style={{marginBottom: "20px"}}>
     
-                  <div class="card bg-light cart" style={{ width: "45vw", marginLeft: "auto",marginRight: "auto" }}>
+                  <div className="card bg-light cart" style={{ width: "45vw", marginLeft: "auto",marginRight: "auto" }}>
                   <article className="proposal-article" style={{maxWidth: "85vw", paddingLeft: "30px", paddingRight: "30px"}}>
-                      <h4 class="card-title mt-3 text-center">Insert a thesis proposal</h4>
-                      <p class="text-center" style={{fontStyle: "italic"}}>Get started with your proposal by inserting your data</p>
-         
-                      <form>
-                      <div class="form-group input-group" style={{ marginTop: "2px", marginBottom: "2px" }}>
-                      <div class="input-group-prepend">
-                        <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: "1vw" }} width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                      <h4 className="card-title mt-3 text-center">Insert a thesis proposal</h4>
+                      <p className="text-center" style={{fontStyle: "italic"}}>Get started with your proposal by inserting your data</p>
+                      <div className="form-group input-group" style={{ marginTop: "2px", marginBottom: "2px" }}>
+                      <div className="input-group-prepend">
+                        <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: "1vw" }} width="16" height="16" fill="currentColor" className="bi bi-info-circle" viewBox="0 0 16 16">
                           <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                           <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
                         </svg>
                       </div>
-                      <input style={{ borderRadius: "6px" }} name="" class="form-control" placeholder="Thesis title" type="text" value={title} onChange={ev => setTitle(ev.target.value)} />
+                      <input style={{ borderRadius: "6px" }} name="" className="form-control" placeholder="Thesis title" type="text" value={title} onChange={ev => setTitle(ev.target.value)} />
                     </div>
-                    <div class="form-group input-group" style={{  marginBottom: "2px" }}>
-                          <div class="input-group-prepend">
-                      <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight:"1vw"}} width="16" height="16" fill="currentColor" class="bi bi-book-fill" viewBox="0 0 16 16">
+                    <div className="form-group input-group" style={{  marginBottom: "2px" }}>
+                          <div className="input-group-prepend">
+                      <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight:"1vw"}} width="16" height="16" fill="currentColor" className="bi bi-book-fill" viewBox="0 0 16 16">
                         <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
                       </svg>
                           </div>
-                          <select class="form-control" style={{borderRadius: "6px"}} value={degree} onChange={ev => setDegree(ev.target.value)}>
+                          <select className="form-control" style={{borderRadius: "6px"}} value={degree} onChange={ev => setDegree(ev.target.value)}>
                               <option value=""> --Type--</option>
                               <option value="Academic Research" >Academic Research</option>
                               <option value="Stage">Stage</option>
 
                           </select>
                       </div>
-                      <div class="form-group input-group" style={{display: "flex", marginBottom: "10px"}}>
+                      <div className="form-group input-group" style={{display: "flex", marginBottom: "10px"}}>
                       <p style={{ paddingTop: "2px", marginBottom: "1px", marginLeft: "auto", marginRight: "auto", fontWeight: "300" }}>Description</p>
                         <textarea
                           style={{fontSize: "15px", width: "60vw", marginLeft: "auto", marginRight: "auto", borderRadius: "1px", fontStyle: "italic", paddingLeft:"5px"}}
@@ -238,30 +236,30 @@ function InsertProposalForm(props) {
                           placeholder="Insert your thesis description.."
                         />
                       </div> 
-                      <div class="form-group input-group" style={{ marginTop: "2px", marginBottom: "2px" }}>
-                      <div class="input-group-prepend">
-                        <svg xmlns="http://www.w3.org/2000/svg"  style={{ marginRight:"1vw"}} width="16" height="16" fill="currentColor" class="bi bi-journal-bookmark-fill" viewBox="0 0 16 16">
-                          <path fill-rule="evenodd" d="M6 1h6v7a.5.5 0 0 1-.757.429L9 7.083 6.757 8.43A.5.5 0 0 1 6 8V1z" />
+                      <div className="form-group input-group" style={{ marginTop: "2px", marginBottom: "2px" }}>
+                      <div className="input-group-prepend">
+                        <svg xmlns="http://www.w3.org/2000/svg"  style={{ marginRight:"1vw"}} width="16" height="16" fill="currentColor" className="bi bi-journal-bookmark-fill" viewBox="0 0 16 16">
+                          <path fillRule ="evenodd" d="M6 1h6v7a.5.5 0 0 1-.757.429L9 7.083 6.757 8.43A.5.5 0 0 1 6 8V1z" />
                           <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z" />
                           <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z" />
                         </svg>
                       </div>
-                      <input style={{ borderRadius: "6px" }} name="" class="form-control" placeholder="Required Knowledge" type="text" value={knowledge} onChange={ev => setKnowledge(ev.target.value)} />
+                      <input style={{ borderRadius: "6px" }} name="" className="form-control" placeholder="Required Knowledge" type="text" value={knowledge} onChange={ev => setKnowledge(ev.target.value)} />
                     </div>
-                      <div class="form-group input-group" style={{ marginTop: '4px'}}>
-                          <div class="input-group-prepend">
-                        <svg xmlns="http://www.w3.org/2000/svg"  style={{ marginRight:"1vw"}} width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-up" viewBox="0 0 16 16">
-                          <path fill-rule="evenodd" d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z" />
+                      <div className="form-group input-group" style={{ marginTop: '4px'}}>
+                          <div className="input-group-prepend">
+                        <svg xmlns="http://www.w3.org/2000/svg"  style={{ marginRight:"1vw"}} width="16" height="16" fill="currentColor" className="bi bi-arrow-bar-up" viewBox="0 0 16 16">
+                          <path fillRule ="evenodd" d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z" />
                         </svg>
                           </div>
-                          <select class="form-control" style={{borderRadius: "6px"}} value={level} onChange={ev => setLevel(ev.target.value)}>
-                              <option selected=""> --Level--</option>
+                          <select className="form-control" style={{borderRadius: "6px"}} value={level} onChange={ev => setLevel(ev.target.value)}>
+                              <option value=""> --Level--</option>
                               <option value= "Master">Master</option>
                               <option value= "Bachelor">Bachelor</option>
 
                           </select>
                       </div>
-                      <div class="form-group input-group" style={{ marginTop: "2px", marginBottom: "2px", display: 'flex', alignItems: 'center' }}>
+                      <div className="form-group input-group" style={{ marginTop: "2px", marginBottom: "2px", display: 'flex', alignItems: 'center' }}>
 
                       <div style={{ display: 'flex', flexDirection: "column", flex: 1 }}>
                         <div className="tags-input-container" style={{ display: 'flex', flexDirection: "row", flexWrap: "wrap" }}>
@@ -273,10 +271,10 @@ function InsertProposalForm(props) {
                           ))}
                         </div>
                         <div style={{display: 'flex', flexDirection: 'row', marginTop: '2px'}}>
-                        <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: "1vw" , marginTop: "2% "}} width="16" height="16" fill="currentColor" class="bi bi-file-word-fill" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: "1vw" , marginTop: "2% "}} width="16" height="16" fill="currentColor" className="bi bi-file-word-fill" viewBox="0 0 16 16">
                           <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5.485 4.879l1.036 4.144.997-3.655a.5.5 0 0 1 .964 0l.997 3.655 1.036-4.144a.5.5 0 0 1 .97.242l-1.5 6a.5.5 0 0 1-.967.01L8 7.402l-1.018 3.73a.5.5 0 0 1-.967-.01l-1.5-6a.5.5 0 1 1 .97-.242z" />
                         </svg>
-                        <input onKeyDown={handleKeyDown} style={{ borderRadius: "3px", marginTop: "2px", marginBottom: "2px", borderWidth: "1px", flex: 1 }} type="text" class="form-control" placeholder="Keywords" />
+                        <input onKeyDown={handleKeyDown} style={{ borderRadius: "3px", marginTop: "2px", marginBottom: "2px", borderWidth: "1px", flex: 1 }} type="text" className="form-control" placeholder="Keywords" />
                         </div>
                       </div>
                     </div>
@@ -306,14 +304,14 @@ function InsertProposalForm(props) {
 
 
 
-                    <div class="form-group input-group" style={{ marginTop: "4px", marginBottom: "2px" }}>
-                      <div class="input-group-prepend">
-                        <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight:"1vw"}} width="16" height="16" fill="currentColor" class="bi bi-person-video3" viewBox="0 0 16 16">
+                    <div className="form-group input-group" style={{ marginTop: "4px", marginBottom: "2px" }}>
+                      <div className="input-group-prepend">
+                        <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight:"1vw"}} width="16" height="16" fill="currentColor" className="bi bi-person-video3" viewBox="0 0 16 16">
                           <path d="M14 9.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm-6 5.7c0 .8.8.8.8.8h6.4s.8 0 .8-.8-.8-3.2-4-3.2-4 2.4-4 3.2Z" />
                           <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h5.243c.122-.326.295-.668.526-1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v7.81c.353.23.656.496.91.783.059-.187.09-.386.09-.593V4a2 2 0 0 0-2-2H2Z" />
                         </svg>
                       </div>
-                      <input style={{ borderRadius: "6px" }} name="" class="form-control" placeholder="Programmes" type="text" value={pname} onChange={ev => setpName(ev.target.value)} />
+                      <input style={{ borderRadius: "6px" }} name="" className="form-control" placeholder="Programmes" type="text" value={pname} onChange={ev => setpName(ev.target.value)} />
                     </div>  
                   <div style={{ display: "flex" }}>
                   <p style={{ paddingTop: "2px", marginBottom: "3px", marginLeft: "auto", marginRight: "auto", fontWeight: "300" }}>Expected Expiration Date (may change)</p>
@@ -326,7 +324,7 @@ function InsertProposalForm(props) {
                     </LocalizationProvider>
                     </div>
                     </div>
-                    <div class="form-group input-group" style={{display: "flex", marginBottom: "2px", flexDirection: "column", flexWrap: "wrap"}}>
+                    <div className="form-group input-group" style={{display: "flex", marginBottom: "2px", flexDirection: "column", flexWrap: "wrap"}}>
                       <p style={{ paddingTop: "2px", marginBottom: "1px", marginLeft: "auto", marginRight: "auto", fontWeight: "300" }}>Notes</p>
                         <textarea
                           style={{fontSize: "15px", width: "100%", marginLeft: "auto", marginRight: "auto", borderRadius: "1px", fontStyle: "italic", paddingLeft:"5px"}}
@@ -339,11 +337,10 @@ function InsertProposalForm(props) {
                       </div> 
                    
                                                        
-                      <div class="form-group" style={{marginTop: "2vh", display: 'flex'}}>
-                          <Button id="sendpb" style={{marginLeft: "auto", marginRight:"auto",  width: "150px", marginBottom: '10px'}} type="submit" class="btn btn-primary btn-block" onClick={handleSubmit}> Upload Proposal  </Button>
+                      <div className="form-group" style={{marginTop: "2vh", display: 'flex'}}>
+                          <Button id="sendpb" style={{marginLeft: "auto", marginRight:"auto",  width: "150px", marginBottom: '10px'}} type="submit" className="btn btn-primary btn-block" onClick={handleSubmit}> Upload Proposal  </Button>
                       </div>     
                                                                                       
-                  </form>
                   </article>
                   </div> 
                   
--- client/src/test/manual/Thesis_Table_FE_test.md
+++ client/src/test/manual/Thesis_Table_FE_test.md
@@ -5,8 +5,8 @@ This file contains a report about the FE testing in the ThesisList component.
 This manual testing will be done as the following:
 
 - Explanation of the procedure used for the test
+- Screenshot of the result of the test  
 - Screenshot of the search bar with wrong data highlighted
-- Screenshot of the result of the test
 - Conclusions
 
 # Functionality Testing
@@ -15,15 +15,20 @@ Data loading works and the student can see a table of proposals:
 
 ![Screenshot](./screenshots/TableProp.png)
 
-By clicking in one of the proposals there's a more complete description.
+At the bottom of the table there are several buttoms that helps to swipe the table in order to see more data. These work well.
+
+![Screenshot](./screenshots/Button.png)
+
+By clicking in one of the proposals inside the table there's a more complete description.
 
 ![Screenshot](./screenshots/Description.png)
 
-Inside the proposal there's a correct division of datas.
+Inside the proposal there's a correct division of data.
 Here the student as the chance to apply by clicking the propriate button.
 The CSS works as well as the mobile one (it has been tested by increasing and decreasing the dimension of the screen). 
 By clicking the apply button there's a working redirection in the form in order to send the student application.
 If the students wants to go back there's a specific button that sends back correctly in the table.
+
 
 
 # Mobile CSS Testing
@@ -40,10 +45,11 @@ The CSS is responsive and it adapts in the different dimensions of the devices:
 ![Screenshot](./screenshots/iPhone12_CSS.png)
 ![Screenshot](./screenshots/Apply_CSS.png)
 
-In the advanced research field the fields change their dimensions:
+In the advanced research field the fields change their dimensions in order to give a tidier display:
 
 Original:
 ![Screenshot](./screenshots/Filters_1.png)
+
 Mobile:
 ![Screenshot](./screenshots/Filters_2.png)
 
