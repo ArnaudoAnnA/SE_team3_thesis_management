b5d91b82144a4cdbb59d4a90d5d8acd5f2e0bcb7
VINCENZO COSI s309332 <s309332@studenti.polito.it>
2023-11-16 00:11:13 +0100

    insertProposal tests

--- client/src/API.js
+++ client/src/API.js
@@ -54,7 +54,9 @@ const isStudent = async (email) => {
   const whereCond = where("email", "==", email)
   const q = query(studentsRef, whereCond)
   const snapshot = await getDocs(q)
-  return snapshot.docs[0] ? true : false
+  if (snapshot.docs[0])
+    return true
+  else return false
 }
 
 /**
@@ -66,7 +68,9 @@ const isTeacher = async (email) => {
   const whereCond = where("email", "==", email)
   const q = query(teachersRef, whereCond)
   const snapshot = await getDocs(q)
-  return snapshot.docs[0] ? true : false
+  if (snapshot.docs[0])
+    return true
+  else return false
 }
 
 /**
@@ -215,7 +219,7 @@ const getAllThesis = async () => {
   * - thesis, contains the array of thesis in case of success, otherwise null.
  */
 async function getThesis(filters) {
-  if (filters === undefined ) {
+  if (filters === undefined) {
     return getAllThesis();
   }
 
@@ -234,29 +238,29 @@ async function getThesis(filters) {
         console.log(expirationDate.from.toISOString());
         whereConditions.push(where('expirationDate', '>=', expirationDate.from.toISOString()));
       }
-    else 
-      console.error('getThesis: Invalid date format in expirationDate.from');
-    
+      else
+        console.error('getThesis: Invalid date format in expirationDate.from');
+
     if (dayjs.isDayjs(expirationDate.to))
       if (expirationDate.to != undefined) {
         console.log(expirationDate.to.toISOString());
-        whereConditions.push(where('expirationDate', '<=', expirationDate.to.toISOString())); 
+        whereConditions.push(where('expirationDate', '<=', expirationDate.to.toISOString()));
       }
-    else
-      console.error('getThesis: Invalid date format in expirationDate.to');
+      else
+        console.error('getThesis: Invalid date format in expirationDate.to');
   }
 
-  if(filters.level != undefined) {
+  if (filters.level != undefined) {
     let level = filters.level;
     whereConditions.push(where('level', '==', level));
   }
 
-  if(filters.keywords != undefined) {
+  if (filters.keywords != undefined) {
     let keywords = filters.keywords;
     whereConditions.push(where('keywords', 'array-contains-any', keywords));
   }
 
-  if(filters.type != undefined) {
+  if (filters.type != undefined) {
     let type = filters.type;
     whereConditions.push(where('type', '==', type));
   }
@@ -272,9 +276,9 @@ async function getThesis(filters) {
         });
         return thesis;
       });
-    if(thesis === undefined) {
+    if (thesis === undefined) {
       return { status: 404, err: 'No thesis found', thesis: [] };
-    }    
+    }
     return { status: 200, thesis: thesis };
   } catch (error) {
     console.log(error);
@@ -339,7 +343,7 @@ const addApplication = async (application) => {
       }
     } else {
       return CONSTANTS.unauthorized
-    } 
+    }
     return;
   }
   else {
@@ -347,143 +351,143 @@ const addApplication = async (application) => {
   }
 }
 
-  /**
-   * Retrieve all the career data of a student by his studentId
-   * @param studentId the id of the student
-   * @return the career ARRAY (look model/Career)
-   */
-  const retrieveCareer = async (studentId) => {
-    console.log("API.retrieveCareer")
-    const whereStudentId = where("id", "==", studentId)
-    const q = query(careersRef, whereStudentId)
-    const careerSnapshot = await getDocs(q)
-    // console.log(careerSnapshot)
+/**
+ * Retrieve all the career data of a student by his studentId
+ * @param studentId the id of the student
+ * @return the career ARRAY (look model/Career)
+ */
+const retrieveCareer = async (studentId) => {
+  console.log("API.retrieveCareer")
+  const whereStudentId = where("id", "==", studentId)
+  const q = query(careersRef, whereStudentId)
+  const careerSnapshot = await getDocs(q)
+  // console.log(careerSnapshot)
 
-    const career = []
-    careerSnapshot.forEach(doc => {
-      const exam = doc.data()
-      // console.log(exam)
-      const obj = new Career(exam.id, exam.codCourse, exam.titleCourse, exam.cfu, exam.grade, exam.date)
-      career.push(obj)
-    })
-    // console.log(career)
-    return career;
-  }
+  const career = []
+  careerSnapshot.forEach(doc => {
+    const exam = doc.data()
+    // console.log(exam)
+    const obj = new Career(exam.id, exam.codCourse, exam.titleCourse, exam.cfu, exam.grade, exam.date)
+    career.push(obj)
+  })
+  // console.log(career)
+  return career;
+}
 
-  /**
-   * Retrieve the thesis title and relative teacher
-   * @param thesisId the id of the thesis
-   * @return an object having 2 properties:
-   *    {
-   *      title : <string>
-   *      teacher : {
-   *                   name: <string>
-   *                   surname: <string>
-   *                }
-   *    }
-   * 
-   */
-  const getTitleAndTeacher = async (thesisId) => {
-    console.log("API.getTitleAndTeacher")
-    const whereThesisId = where("id", "==", Number(thesisId))
-    const qThesis = query(thesisProposalsRef, whereThesisId)
-    try {
-      const thesisSnapshot = await getDocs(qThesis)
-      const thesis = thesisSnapshot.docs[0].data()
-      const whereTheacherId = where("id", "==", thesis.teacherId)
-      const qTeacher = query(teachersRef, whereTheacherId)
-      const teacherSnapshot = await getDocs(qTeacher)
-      const teacher = teacherSnapshot.docs[0].data()
+/**
+ * Retrieve the thesis title and relative teacher
+ * @param thesisId the id of the thesis
+ * @return an object having 2 properties:
+ *    {
+ *      title : <string>
+ *      teacher : {
+ *                   name: <string>
+ *                   surname: <string>
+ *                }
+ *    }
+ * 
+ */
+const getTitleAndTeacher = async (thesisId) => {
+  console.log("API.getTitleAndTeacher")
+  const whereThesisId = where("id", "==", Number(thesisId))
+  const qThesis = query(thesisProposalsRef, whereThesisId)
+  try {
+    const thesisSnapshot = await getDocs(qThesis)
+    const thesis = thesisSnapshot.docs[0].data()
+    const whereTheacherId = where("id", "==", thesis.teacherId)
+    const qTeacher = query(teachersRef, whereTheacherId)
+    const teacherSnapshot = await getDocs(qTeacher)
+    const teacher = teacherSnapshot.docs[0].data()
 
-      return {
-        title: thesis.title,
-        teacher: {
-          name: teacher.name,
-          surname: teacher.surname
-        }
+    return {
+      title: thesis.title,
+      teacher: {
+        name: teacher.name,
+        surname: teacher.surname
       }
-
-    } catch (e) {
-      console.log(e)
     }
 
-    return;
+  } catch (e) {
+    console.log(e)
   }
 
-  /**
-   * Retrieve the application by the id of the student and the id of the thesis
-   * @param studentId the id of the student
-   * @param thesisId the id of the thesis
-   * @return the application object, null if the object doesn't exist
-   * 
-   */
-  const getApplication = async (studentId, thesisId) => {
-    if (auth.currentUser) {
-      if (StringUtils.checkId(studentId, auth.currentUser.email)) {
-        console.log(studentId)
-        console.log(thesisId)
-        const whereThesisId = where("thesisId", "==", Number(thesisId))
-        const whereStudentId = where("studentId", "==", studentId)
+  return;
+}
 
-        const qApplication = query(applicationsRef, whereThesisId, whereStudentId)
-        // console.log(qApplication)
-        try {
-          const applicationSnapshot = await getDocs(qApplication)
-          applicationSnapshot.forEach(e => { console.log(e) })
-          if (applicationSnapshot.docs.length > 0) {
-            console.log("there is already a record")
-            const app = applicationSnapshot.docs[0].data()
-            return new Application(app.studentId, app.thesisId, app.accepted, app.curriculum, app.date)
-          }
-          console.log("No records")
-          return null
-        } catch (error) {
-          console.log(e)
+/**
+ * Retrieve the application by the id of the student and the id of the thesis
+ * @param studentId the id of the student
+ * @param thesisId the id of the thesis
+ * @return the application object, null if the object doesn't exist
+ * 
+ */
+const getApplication = async (studentId, thesisId) => {
+  if (auth.currentUser) {
+    if (StringUtils.checkId(studentId, auth.currentUser.email)) {
+      console.log(studentId)
+      console.log(thesisId)
+      const whereThesisId = where("thesisId", "==", Number(thesisId))
+      const whereStudentId = where("studentId", "==", studentId)
+
+      const qApplication = query(applicationsRef, whereThesisId, whereStudentId)
+      // console.log(qApplication)
+      try {
+        const applicationSnapshot = await getDocs(qApplication)
+        applicationSnapshot.forEach(e => { console.log(e) })
+        if (applicationSnapshot.docs.length > 0) {
+          console.log("there is already a record")
+          const app = applicationSnapshot.docs[0].data()
+          return new Application(app.studentId, app.thesisId, app.accepted, app.curriculum, app.date)
         }
-      } else {
-        return CONSTANTS.unauthorized
+        console.log("No records")
+        return null
+      } catch (error) {
+        console.log(e)
       }
     } else {
-      return CONSTANTS.notLogged
+      return CONSTANTS.unauthorized
     }
-
-    return;
+  } else {
+    return CONSTANTS.notLogged
   }
 
-  /*only for testing purposes*/
-  /**
-   * @return the number of proposals deleted
-   */
-  const removeAllProposals = async () => {
+  return;
+}
 
-    const q = query(thesisProposalsRef);
+/*only for testing purposes*/
+/**
+ * @return the number of proposals deleted
+ */
+const removeAllProposals = async () => {
 
-    try {
+  const q = query(thesisProposalsRef);
 
-      const qSnapShot = await getDocs(q);
-      const ids = [];
+  try {
 
-      qSnapShot.forEach(doc => {
-        ids.push(doc.id);
-      });
+    const qSnapShot = await getDocs(q);
+    const ids = [];
 
-      const len = ids.length;
-      const collectionName = DEBUG ? "test-thesisProposals" : "thesisProposals";
+    qSnapShot.forEach(doc => {
+      ids.push(doc.id);
+    });
 
-      ids.forEach(async (id) => {
-        const docRef = doc(db, collectionName, id);
-        await deleteDoc(docRef);
-      })
+    const len = ids.length;
+    const collectionName = DEBUG ? "test-thesisProposals" : "thesisProposals";
 
-      console.log(len);
-      return len;
-    } catch (e) {
-      console.log(e)
-    }
+    ids.forEach(async (id) => {
+      const docRef = doc(db, collectionName, id);
+      await deleteDoc(docRef);
+    })
 
-  };
+    console.log(len);
+    return len;
+  } catch (e) {
+    console.log(e)
+  }
 
-  //Structure of a correct thesis proposal
+};
+
+//Structure of a correct thesis proposal
 const predefinedProposalStructure = {
   archiveDate: "",
   coSupervisors: [],
@@ -499,7 +503,7 @@ const predefinedProposalStructure = {
   teacherId: "",
   title: "",
   type: "",
-  };
+};
 
 // Sample data representing a thesis proposal
 /*const thesisProposalData = {
@@ -520,25 +524,29 @@ const predefinedProposalStructure = {
 };*/
 
 const insertProposal = async (thesisProposalData) => {
+  if (!auth.currentUser) return { status: 401, err: "User not logged in" };
+  if (!isTeacher(auth.currentUser.email)) return { status: 401, err: "User is not a teacher" };
+
+
   const validateThesisProposalData = (data) => {
 
     //Validation that the proposal meets the structure requirements
     const keys1 = Object.keys(thesisProposalData);
     const keys2 = Object.keys(predefinedProposalStructure);
-    
-      // Check if both objects have the same number of keys
-      if (keys1.length !== keys2.length) {
-        console.log("part1")
+
+    // Check if both objects have the same number of keys
+    if (keys1.length !== keys2.length) {
+      console.log("part1")
+      return false;
+    }
+    // Check if all keys in obj1 exist in obj2 and have the same type
+    for (const key of keys1) {
+      if (!(key in predefinedProposalStructure) || typeof thesisProposalData[key] !== typeof predefinedProposalStructure[key]) {
+        console.log("part2")
+        console.log(key)
         return false;
       }
-      // Check if all keys in obj1 exist in obj2 and have the same type
-      for (const key of keys1) {
-        if (!(key in predefinedProposalStructure) || typeof thesisProposalData[key] !== typeof predefinedProposalStructure[key]) {
-          console.log("part2")
-          console.log(key)
-          return false;
-        }
-      }  
+    }
 
     //null values validation
     const keys = Object.keys(data);
@@ -553,22 +561,22 @@ const insertProposal = async (thesisProposalData) => {
 
   if (!validateThesisProposalData(thesisProposalData)) {
     console.log("Validation failed: proposal data doesnt comply with required structure");
-    return null;
+    return { status: 400, err: "Proposal data doesnt comply with required structure" };
   }
 
   //Check that the teachers id is an id inside the teachers table
   if (!isTeacherById(thesisProposalData.id)) {
     console.log("Validation failed: the proposed teacher is not present in our database");
-    return null;
+    return { status: 400, err: "The proposed teacher is not present in our database" };
   }
 
   try {
     const docRef = await addDoc(thesisProposalsRef, thesisProposalData);
     console.log("Thesis proposal added with ID: ", docRef.id);
-    return docRef.id;
+    return { status: 200, id: docRef.id };
   } catch (error) {
     console.error("Error adding thesis proposal: ", error);
-    return null; // or handle the error accordingly
+    return { status: 500 }; // or handle the error accordingly
   }
 };
 
