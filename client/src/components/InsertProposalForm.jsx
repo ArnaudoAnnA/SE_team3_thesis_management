f747e84a7229c09af578d5f69aa16671958b9372
Emilio Cassaro <s319083@studenti.polito.it>
2023-11-15 21:59:52 +0100

    Fixing trim mail bug

--- client/src/components/InsertProposalForm.jsx
+++ client/src/components/InsertProposalForm.jsx
@@ -47,7 +47,7 @@ function InsertProposalForm(props) {
   function handleKeyDown(e){
     if(e.key !== 'Enter') return
     e.preventDefault(); 
-    const value = e.target.value
+    const value = e.target.value.trim();
     if(!value.trim()) return
     if(!tags.includes(value)) {
       setTags([...tags, value])
@@ -65,7 +65,7 @@ function InsertProposalForm(props) {
   }
   
   function handleBlur(e) {
-    const value = e.target.value;
+    const value = e.target.value.trim();
     if (!value.trim()) return;
     if (!tags.includes(value)) {
       setTags([...tags, value]);
@@ -82,7 +82,7 @@ function InsertProposalForm(props) {
     e.preventDefault();
     const value = e.target.value.trim();
     
-    if (!value) return;
+    if (!value.trim()) return;
   
     if (!value.match(mailRegex)) {
       setErrorMsg('Not a valid email address.');
@@ -92,6 +92,7 @@ function InsertProposalForm(props) {
   
     if (!emailTags.includes(value)) {
       setEmailTags([...emailTags, value]);
+   
     }else{
       setErrorMsg('Co-Supervisor already exists.');
       window.scrollTo(0, 0);
@@ -361,7 +362,7 @@ function InsertProposalForm(props) {
                         <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: "1vw" , marginTop: "2% "}} width="16" height="16" fill="currentColor" className="bi bi-file-word-fill" viewBox="0 0 16 16">
                           <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5.485 4.879l1.036 4.144.997-3.655a.5.5 0 0 1 .964 0l.997 3.655 1.036-4.144a.5.5 0 0 1 .97.242l-1.5 6a.5.5 0 0 1-.967.01L8 7.402l-1.018 3.73a.5.5 0 0 1-.967-.01l-1.5-6a.5.5 0 1 1 .97-.242z" />
                         </svg>
-                        <input onKeyDown={handleKeyDown}     onBlur={handleBlur} style={{ borderRadius: "3px", marginTop: "2px", marginBottom: "2px", borderWidth: "1px", flex: 1 }} type="text" className="form-control" placeholder="Insert Keywords and press Enter.." />
+                        <input onKeyDown={handleKeyDown}  onBlur={handleBlur} style={{ borderRadius: "3px", marginTop: "2px", marginBottom: "2px", borderWidth: "1px", flex: 1 }} type="text" className="form-control" placeholder="Insert Keywords and press Enter.." />
                         </div>
                       </div>
                     </div>
@@ -384,7 +385,7 @@ function InsertProposalForm(props) {
                             <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2H2Zm-2 9.8V4.698l5.803 3.546L0 11.801Zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 9.671V4.697l-5.803 3.546.338.208A4.482 4.482 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671Z" />
                             <path d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034v.21Zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791Z" />
                           </svg>
-                          <input style={{ borderRadius: '6px', flex: 1 }} name="" className="form-control" placeholder="Insert Co-Supervisors' mails and press Enter.." type="text" value={email} onKeyDown={handleMailKeyDown}  onBlur={handleMailBlur} onChange={(ev) => setEmail(ev.target.value)} />
+                          <input style={{ borderRadius: '6px', flex: 1 }} name="" className="form-control" placeholder="Insert Co-Supervisors' mails and press Enter.." type="text" onKeyDown={handleMailKeyDown}  onBlur={handleMailBlur} />
                         </div>
                       </div>
                     </div>
