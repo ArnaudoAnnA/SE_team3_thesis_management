e0440577bd1d1543438d95d84358633d6de22ed9
dvylch <chndavide@gmail.com>
2023-11-16 12:31:37 +0100

    add file to application

--- client/src/utils/StringUtils.js
+++ client/src/utils/StringUtils.js
@@ -1,9 +1,15 @@
 const checkId = (id, email) => {
     return email.toLowerCase().includes(id.toLowerCase())
 }
-
-const StringUtils = {
-    checkId
+const createApplicationPath = (dir, user, thesisId, file ) => {
+    const path = dir + user + "_" + thesisId + "/" + file
+    return path
 }
+const StringUtils = {
+    checkId,
+    createApplicationPath
+}
+
+
 
 export default StringUtils
