de0472c083e0ae14468ca6f43b45390455e8df58
ArnaudoAnnA <38939245+ArnaudoAnnA@users.noreply.github.com>
2023-11-25 08:03:01 +0100

    gui improvements + integration with filters form

--- client/src/components/ThesisList/FiltersForm.jsx
+++ client/src/components/ThesisList/FiltersForm.jsx
@@ -1,7 +1,7 @@
 
 import {Row, Col, Button, Form, Table} from "react-bootstrap";
 import { Search, Filter } from "react-bootstrap-icons";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { userContext } from "../Utils";
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
@@ -22,13 +22,10 @@ function ThesisFieldFilterForm(props)
                 </>;
         default:
             return <Autocomplete
-            options={API.getValuesForField(props.DBfield).map(e => {return {value: e, label: e};})}
+            options={API.getValuesForField(props.DBfield) /*.map(e => {return {value: e, label: e};}) */}
             autoComplete
             autoSelect
             onChange={(event) => {
-                props.onChangeFiltersForm(props.DBfield, event.target.outerText);
-            }}
-            onBlur={(event) => {
                 props.onChangeFiltersForm(props.DBfield, event.target.outerText);
             }}
             renderInput={(params) => <TextField {...params} variant="standard"   style={{ paddingLeft: "2px", borderRadius: "6px", width: '100%', fontSize: "12px"}}/>}
@@ -71,6 +68,7 @@ function FiltersForm(props)
 {
     let [filters, setFilters, resetFilters, isFiltered] = props.filters;
     const [showAdvancedFilters, setShowAdvancedFilters] = useState(false);
+    const [mediaLarge, setMediaLarge] = useState(window.matchMedia("(min-width: 700px)").matches);
     const ctxState = useContext(contextState);
 
     /* --------- FUNCTIONS ------------------- */
@@ -98,13 +96,23 @@ function FiltersForm(props)
 
     /* ------------------------------------ */
 
+    useEffect(() =>
+    {
+        setMediaLarge(window.matchMedia("(min-width: 700px)").matches);
+    }, []);
 
     return (
         <>
         <Row className="mb-3 justify-content-around">
             <Col className="col-xl-9 col-lg-9 col-md-9 col-s-9 col-xs-8"><Form.Control value={props.filters.title} id='title' type="text" placeholder="Search title..." onChange={(event) => onChangeFiltersForm(event)} /></Col>
             <Col className="col-1" style={{backgroundColor: "#fff0"}}><Search className="flexible_icons icons" onClick={() => ctxState.setState(ctxState.states.loading)}/></Col>
-            <Col className="col-2" style={{backgroundColor: "#fff0"}}><Button onClick={() => setShowAdvancedFilters(s => !s)}>More filters...</Button></Col>
+            <Col className="col-2" style={{backgroundColor: "#fff0"}}>
+                {
+                    mediaLarge ?
+                    <Button onClick={() => setShowAdvancedFilters(s => !s)}>More filters...</Button>
+                    : <Filter className="flexible_icons icons" onClick={() => setShowAdvancedFilters(s => !s)} />
+                }
+            </Col>
         </Row>
         <Row>
             {showAdvancedFilters ? <AdvancedFiltersTable filters={filters} onChangeFiltersForm={onChangeFiltersForm}/>
